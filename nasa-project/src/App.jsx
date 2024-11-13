8314471a7daa3d4be6871834033523f84e9a9d42
José Aldenir Almeida Gomes Filho <jose@layoutsistemas@gmail.com>
2024-11-13 11:23:29 -0300

    ADS-filtro_data: 01697549 Reactjs-vite

--- nasa-project/src/App.jsx
+++ nasa-project/src/App.jsx
@@ -2,22 +2,28 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [date, setDate] = useState('2023-11-22'); 
   const [imageData, setImageData] = useState(null);
 
   useEffect(() => {
     const fetchImage = async () => {
-      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=O42i4uno6c2kebzqhLBIF3cD7Ld2q4wtrc8gWgy8`);
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=O42i4uno6c2kebzqhLBIF3cD7Ld2q4wtrc8gWgy8&date=${date}`);
+      console.log(date)
       const data = await response.json();
       setImageData(data);
+      console.log(imageData.url)
     };
 
     fetchImage();
-  },);
+  },[date]);
 
 
 
   return (
     <div>
+      <h1>ESCOLHA UMA DATA</h1>
+      <input type="date" className='date-field'  value={date} onChange={(e) => setDate(e.target.value)} />
+      <hr />
       {imageData && (
         <div>
           <h2>{imageData.title}</h2>
@@ -34,7 +40,7 @@ export default App;
 
 /**
  * Nome do arquivo: App.jsx
- * Data de criação: 12/11/2024
+ * Data de criação: 13/11/2024
  * Autor: José Aldenir Almeida Gomes Filho
  * Matrícula: 01697549
  *
