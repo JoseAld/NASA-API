051b44488f2768c29428201eafa2cdf0911318a0
José Aldenir Almeida Gomes Filho <jose@layoutsistemas@gmail.com>
2024-11-13 11:02:41 -0300

    ADS-imagem_do_dia: 01697549  Reactjs-vite

--- nasa-project/src/App.jsx
+++ nasa-project/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageData, setImageData] = useState(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=O42i4uno6c2kebzqhLBIF3cD7Ld2q4wtrc8gWgy8`);
+      const data = await response.json();
+      setImageData(data);
+    };
+
+    fetchImage();
+  },);
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {imageData && (
+        <div>
+          <h2>{imageData.title}</h2>
+          <img src={imageData.url} className='image' />
+          <p>{imageData.explanation}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+/**
+ * Nome do arquivo: App.jsx
+ * Data de criação: 12/11/2024
+ * Autor: José Aldenir Almeida Gomes Filho
+ * Matrícula: 01697549
+ *
+ * Descrição:
+ * Este arquivo REACT é responsável por implementar as funcionalidades,
+ * constantes, a url de API e seus parametros, e retornar para o usuario oque é solicitado ao sistema
+ */
--- nasa-project/src/main.jsx
+++ nasa-project/src/main.jsx
@@ -8,3 +8,15 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+
+/**
+ * Nome do arquivo: main.jsx
+ * Data de criação: 13/11/2024
+ * Autor: José Aldenir Almeida Gomes Filho
+ * Matrícula: 01697549
+ *
+ * Descrição:
+ * Este arquivo REACT é responsável por mostar nas telas os componentes, partes de código
+ * atuando como corpo principal ,
+ */
